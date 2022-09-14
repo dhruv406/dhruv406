44bb0de4eff7ded04661353b7a686b238e66093c
dhruv406 <113508752+dhruv406@users.noreply.github.com>
2022-09-14 17:41:51 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+  int main() {
+  int a, b;
+
+  printf("Enter your Keywords a\n");
+  scanf("%d", &a);
+
+  printf("Enter your keywords b\n");
+  scanf("%d", &b);
+
+  printf(" sum of a & b is:- %d\n", a+b);
+  return 0;
+  }
